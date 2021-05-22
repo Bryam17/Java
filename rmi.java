fe1c972a155a6142d8553a1f418296e567d2af40
Bryam17 <bryamvargas64@gmail.com>
2021-05-21 20:37:05 -0600

    orignal

---
+++ rmi.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.calculadorarmi;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ *
+ * @author bryam
+ */
+public class rmi extends UnicastRemoteObject implements Interfaces{
+
+    public rmi() throws RemoteException{
+        int a, b;
+    }
+
+    @Override
+    public int suma(int a, int b) throws RemoteException {
+        return (a + b);
+    }
+
+    @Override
+    public int resta(int a, int b) throws RemoteException {
+        return (a - b);
+    }
+
+    @Override
+    public int mult(int a, int b) throws RemoteException {
+        return (a * b);
+    }
+
+    @Override
+    public int div(int a, int b) throws RemoteException {
+       return (a / b);
+    }
+    
+    
+    
+}
